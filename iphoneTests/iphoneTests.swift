aff8fff90832335805c0c184608a08b7c30bbab1
bhaveekjain <jainbhaveek.v727@gmail.com>
2026-01-12 23:17:41 +0530

    Initial Commit

---
+++ iphoneTests/iphoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  iphoneTests.swift
+//  iphoneTests
+//
+//  Created by Bhaveek Jain on 12/01/26.
+//
+
+import Testing
+@testable import iphone
+
+struct iphoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
